b60f464b66bcddc32b91487714430ec9a601eec0
Andrey Tarasenko <antarasenko123@gmail.com>
2022-05-06 11:34:41 +0300

    05/05/22 test commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mypage</title>
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+    <img src="images/logo.png">
+
+    <nav class="main">
+            <div class="razdel"><a href="#" target="_blank">Главная</a></div>
+            <div class="razdel"><a href="table.html" target="_blank">Таблица Менделеева</a></div>
+            <div class="razdel"><a href="flex.html" target="_blank">Пример работы Flex</a></div>
+            <div class="razdel"><a href="grid.html" target="_blank">Пример работы Grid</a></div>
+    </nav>
+
+<!--    <nav class="main">
+        <ul>
+            <li><a href="#" target="_blank">Главная</a></li>
+            <li><a href="table.html" target="_blank">Таблица Менделеева</a></li>
+        </ul>
+    </nav>-->
+</header>
+
+<div class="photo">
+    <img src="images/DSC072291221234.jpg" width="300px">
+</div>
+
+<div class="fio"><h2>Тарасенко Андрей Юрьевич</h2></div>
+<div class="info">Добрый день, меня зовут Андрей. Живу в Москве, работаю в сфере ИТ более 15 лет, интересуюсь <b>веб-разработкой</b>.</div>
+<div class="mnenie">Известная студия веб-разработки и создания сайтов на Битрикс, хорошая школа, профессиональные преподаватели!</div>
+
+</body>
+</html>
