be84afcdfa35074ab9ee0dfae4cf1c2bab3d4604
Пользователь <Shondin07@mail.ru>
2022-05-08 16:11:43 +0500

    DZ ot 05.05.2022

---
+++ dz_ot_05_05_2022/index.php
@@ -0,0 +1,286 @@
+<!doctype html>
+<head>
+<link rel="stylesheet" href="style/style_index.css">
+<link rel="stylesheet" href="style/style_FLEXBOX.css">
+<link rel="stylesheet" href="style/style_GRID.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- это для меню -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<!-- от меню-->
+<title> Шондин А.С. </title>
+</head>
+<body>
+<header>
+  <!-- верхняя часть с логотипом -->
+<div class=blok-anketa">
+<!-- общий блок с анкетой-->
+<header>
+  <div class="blok_shapka">     <img class="logo" src="./images/logo.png" alt="Лого"> <div>
+</header>
+</header>
+<!-- nav - блок меню-->
+<nav>
+<div class="blok-menu">
+  <!-- меню брал от сюда https://dbmast.ru/files/responsive-menu/#-->
+  <nav class="nav">
+    <ul>
+      <li class="current"><a href="#">Главная</a></li>
+      <li><a href="./tablisa_mendeleeva.html">Таблица Менделеева</a></li>
+      <li><a href="#">пункт 3</a></li>
+    </ul>
+  </nav>
+  </nav>
+<di>
+  <!-- main это основная часть -->
+<div class="podblok-grid">
+<div class="blok-photo">  <!-- блок сс фотографикй-->
+<img class="photo" src="./images/Shondin.jpg" alt="Лого">
+</div>
+<div class="blok-name">  <!-- блок с именем-->
+  <h1>Шондин Андрей</h1>
+</div>
+<div class="blok-info">  <!-- блок с рассказом-->
+  Привет! Расскажу немного о себе. Отучился на программиста, но по специальности не работал. Создал несколько сайтов под собственные нужды на Wordpress и Opencart.<br> Мои сайты: <a href="https://na-bannoe.ru/">na-bannoe.ru</a>, <a href="https://choc-ariba.ru/">choc-ariba.ru </a>.
+    <p> Люблю активный отдых.</p>
+</div>
+<div class="blok-otziv">  <!-- блок с отзывом-->         Выступаю за то, что бы все работали в одном редакторе кода, т.к. у каждого есть свои особенности.</div>
+</div> <!-- закрываем бюлок анкета-->
+</div> <!-- закрываем подблок с гридом -->
+    <div class="flex-grid">
+  <div class="blok-flex">
+    <h1>FLEX</h1>
+   <h2>1 схема</h2>
+    </p>
+    <div class="blok-flex1">
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil1.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            Смайлик с шляпой
+          </div>
+        </container>
+      </div>
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil2.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            Смущающийся смайлик
+          </div>
+        </container>
+      </div>
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil3.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            удивленный смайлик
+          </div>
+        </container>
+      </div>
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil4.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            смеющийся смайлик
+          </div>
+        </container>
+      </div>
+    </div>
+  <h1>2 схема</h1>
+    <div class ="blok-flex2-shema2">
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil1.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            Смайлик с шляпой
+          </div>
+        </container>
+      </div>
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil2.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            Смущающийся смайлик
+          </div>
+        </container>
+      </div>
+      <div class="break"></div> <!--ПЕРЕНОС НА НОВУЮ СТРОКУ-->
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil3.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            удивленный смайлик
+          </div>
+        </container>
+      </div>
+      <div class="blok-flex2">
+        <container class="flex">
+          <div class="verh">
+            <img class="kartinka-verh " src="images/smil4.png" alt="смайлик 1">
+          </div>
+          <div class="niz">
+            смеющийся смайлик
+          </div>
+        </container>
+      </div>
+    </div>
+  </div>
+<h1>GRID</h1>
+<h2>1 схема</h2>
+  </p>
+  <div class="blok-grid1">
+    <div class="blok-grid2">
+      <div class="verh">
+        <img class="kartinka-verh " src="images/smil1.png" alt="смайлик 1">
+      </div>
+      <div class="niz">
+        Смайлик с шляпой
+      </div>
+    </div>
+    <div class="blok-grid2">
+      <div class="verh">
+        <img class="kartinka-verh " src="images/smil2.png" alt="смайлик 1">
+      </div>
+      <div class="niz">
+        Смущающийся смайлик
+      </div>
+    </div>
+    <div class="blok-grid2">
+      <div class="verh">
+        <img class="kartinka-verh " src="images/smil3.png" alt="смайлик 1">
+      </div>
+      <div class="niz">
+        удивленный смайлик
+      </div>
+    </div>
+    <div class="blok-grid2">
+      <div class="verh">
+        <img class="kartinka-verh " src="images/smil4.png" alt="смайлик 1">
+      </div>
+      <div class="niz">
+        смеющийся смайлик
+      </div>
+    </div>
+  </div>
+  <center><h1>2 схема</h1></center>
+  <div class="blok-grid2-shema2">
+    <div class="blok1">
+      <div class="blok-grid2">
+        <div class="verh">
+          <img class="kartinka-verh " src="images/smil1.png" alt="смайлик 1">
+        </div>
+        <div class="niz">
+          Смайлик с шляпой
+        </div>
+
+      </div>
+    </div>
+    <div class="blok2">
+      <div class="blok-grid2">
+        <div class="verh">
+          <img class="kartinka-verh " src="images/smil2.png" alt="смайлик 1">
+        </div>
+        <div class="niz">
+          Смущающийся смайлик
+        </div>
+      </div>
+    </div>
+    <div class="blok3">
+      <div class="blok-grid2">
+        <div class="verh">
+          <img class="kartinka-verh " src="images/smil3.png" alt="смайлик 1">
+        </div>
+        <div class="niz">
+          удивленный смайлик
+        </div>
+      </div>
+    </div>
+    <div class="blok4">
+      <div class="blok-grid2">
+        <div class="verh">
+          <img class="kartinka-verh " src="images/smil4.png" alt="смайлик 1">
+        </div>
+        <div class="niz">
+          Смеющийся смайлик
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+    </div>
+    <?php
+
+
+    $vremya_khasov= date('H'); // записываем в переменную $vremya_khasov количество часов
+    $vremya_khasov=$vremya_khasov+2; // Что бы время было по Магнитогорскому времени ( а не по Москве)
+    //    echo $vremya_khasov;
+    if ($vremya_khasov<19 and $vremya_khasov>7 ) {    // дневная тема
+//         echo "дневная тема";
+        echo "<style>
+        * {
+        color: #0e0101; /* Цвет символа */
+        }   
+        .blok_shapka {
+            background-color: #f5cf9f;
+        }
+        .blok-menu {
+        background-color: #e8bc81;
+        }
+        .podblok-grid {
+        background-color: #eadbb9;
+        }
+        a {
+        color: #69C;
+        }
+        .flex-grid {
+        background-color: #eadbb9;
+        }
+        </style>";
+    } else {                                          // ночная тема
+//         echo "ночная тема";
+        echo "<style>
+        * {
+        color: #fffcfc; /* Цвет символа */
+        }   
+        .blok_shapka {
+            background-color: black;
+        }
+                .blok-menu {
+        background-color: #5e5a59;
+        }
+                .podblok-grid {
+        background-color: #7b7a75;
+        }
+                a {
+        color: #a5caef;
+        }
+               .flex-grid {
+         background-color: #7b7a75;
+        }
+        </style>";
+
+    }
+    echo $a;
+    ?>
+</main>
+<footer>
+  <!-- подвал сайта -->
+</footer>
+
+
+
+
+</body>
