b122c540b670116a468c5e21baf3e75912bb0d9c
Shondin Andrey <Shondin07@mail.ru>
2022-05-15 13:43:40 +0500

    DZ ot 12.05.2022

--- dz/bloki/header_verh_saita.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="./style/style_index.css">
-<header>
-  <!-- верхняя часть с логотипом -->
-<div class=blok-anketa">
-<!-- общий блок с анкетой-->
-  <div class="blok_shapka">     <img class="logo" src="./images/logo.png" alt="Лого"> <div>
-</header>
