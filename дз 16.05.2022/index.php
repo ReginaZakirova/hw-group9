db50efa496964f7a0a4da079756364b331428d9d
Anton Markov <wed103@mail.ru>
2022-05-19 13:00:02 +0500

    дз 16.05.2022

--- дз 16.05.2022/index.php
+++ дз 16.05.2022/index.php
@@ -4,13 +4,6 @@
     <meta charset="UTF-8">
     <meta name="keywords" content="Курсы, обучение">
     <link rel="stylesheet" href="style/style.css">
-    <?php
-        $time = getdate();
-        $time = $time["hours"];
-        if ($time < 8 || $time >= 20) {
-            echo "<link rel='stylesheet' href='style/styleNight.css'>";
-        }
-    ?>
     <title>Домашняя работа 1</title>
 </head>
 <body class="night">
@@ -138,6 +131,113 @@
                 <span>Вид со смотровой в Алании.</span>
             </div>
         </div>
+    <?php
+        $time = getdate();
+        $time = $time["hours"];
+        if ($time < 0 || $time >= 20) {
+            echo "<link rel='stylesheet' href='style/styleNight.css'>";
+        }
+    ?>
+    <?php
+        echo "задача 4 слайд 20" . '<br>';
+        $a = 'html css php';
+        $arr = explode(" ",$a);
+        print_r($arr);
+        
+        echo "задача 5 слайд 20" . '<br>';
+        $a = "27.03.1991";
+        $b = "18.05.2022";
+        $arrMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        $n = 0;
+        $NleapYear = 8;
+        $arr2 = explode(".",$a);
+        $arr3 = explode(".",$b);
+        print_r($arr2);
+        echo "<br>";
+        print_r($arr3);
+        echo "<br>";
+        for ($i = 0; $i < 3; $i++) {
+            $arr4[$i] = $arr3[$i] - $arr2[$i];        
+        }
+        print_r($arr4);
+        echo '<br>';
+        for ($i = 0; $i < abs($arr3[1] - $arr2[1]); $i++) {
+            if ($arr3[1] - $arr2[1] > 0) {
+                $month[$i] = $arr2[1] + $i;
+            }
+            else {
+                $month[$i] = (11 - abs($arr3[1] - $arr2[1])) + $i;  //запутался, тут еще нужно учесть естли $month[$i] станет больше 11 то снова с 0
+                                                                    // эту задачу реализовал по другому в конце дз
+            }
+        }
+        print_r($month) . '<br>';
+
+        for ($i = 0; $i < abs($arr3[1] - $arr2[1]); $i++) {  
+            $monthSum = $monthSum + $arrMonth[$month[$i]];     
+            $n = $arr4[2]*365 + $monthSum + $arr4[0];
+        }
+        echo abs($n) + $NleapYear . "<br>";
+
+
+        echo "дз пункт 1" . '<br>';
+        $str = file_get_contents('index.php');
+        $str1 = file_get_contents('header.php');
+
+        $a = strip_tags($str);
+        $arr = explode('.', $a);
+        $arr[0] = "<span class='replace'>$arr[0]</span>";
+        $b = implode($arr);
+        echo $b;
+
+        $b = mb_substr($arr[0], -12);
+        echo str_replace($b, '<span class="replace">Здравствуйте</span>', $b);   //так и не понял в какой момент содержимое изначальной строки должно изменится.
+       
+        echo "дз пункт 3" . '<br>';
+
+        $arr2 = explode(' ', $a);
+        $c = strip_tags($str1);
+        echo "количество слов на странице = ";
+        echo str_word_count($a, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя") + str_word_count($c, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя");
+        echo '<br>';
+
+        echo "дз пункт 2" . '<br>';
+        $arrA = ['а','у','о','ы','э','я','ю','ё','и','е'];
+        $arr6 = mb_str_split($a);
+        for ($i = 0; $i < count($arrA); $i++) {
+            for ($j = 0; $j < count($arr6); $j++) {
+                if ($arrA[$i] == $arr6[$j]) {
+                    $count++;
+                }
+            }
+        }
+        echo $count . "<br>";
+
+        echo "дз пункт 4" . '<br>';
+        echo '<form method="post">
+                    <label for = "date">Введите дату рождения</label>
+                    <input type = "date" id="date" name="user">
+                    <input type="submit" value="отправить">
+              </form>';
+              echo $_POST['user'] . '<br>';
+        $date = $_POST['user'];
+        $dateToday = date('Y-m-d');
+        echo $dateToday . '<br>';
+        $NleapYear = 8;
+        $arr8 = explode("-",$date);
+        $arr9 = explode("-",$dateToday);
+        $daysBeginningYear1 = $arr8[2];
+        $daysBeginningYear2 = $arr9[2];
+            for ($i = 0; $i < (int)$arr8[1]; $i++) {
+                $daysBeginningYear1 = $daysBeginningYear1 + $arrMonth[$i];
+            };
+            for ($i = 0; $i < (int)$arr9[1]; $i++) {
+                $daysBeginningYear2 = $daysBeginningYear2 + $arrMonth[$i];
+            };
+        echo $daysBeginningYear1 . '<br>';
+        echo $daysBeginningYear2 . '<br>';
+        $Ndays = abs(($arr9[0] - $arr8[0])*365 + ($daysBeginningYear2 - $daysBeginningYear1)) ;
+        echo $Ndays + $NleapYear;
+    ?>
     </main>
     <?php
         require "footer.php";
